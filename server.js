daebf374a35094dcdd15dc08dfcd7717f6dc8a27
Lamriver123 <ngoclamhttv@gmail.com>
2025-09-14 00:01:14 +0700

    "Hi"

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const path = require('path');
 const app = express();
 
 // Thư mục build Angular browser
-const browserDist = path.join(__dirname, './dist/import_product_fe/browser');
+const browserDist = path.join(__dirname, 'dist/import_product_fe/browser');
 
 app.use(express.static(browserDist));
 
-// Route fallback cho Angular SPA
-app.get('*', (req, res) => {
+// Fallback cho Angular SPA (dùng /* thay vì *)
+app.use((req, res) => {
   res.sendFile(path.join(browserDist, 'index.html'));
 });
 
-// Cổng do Azure App Service cấp qua biến môi trường
+
+// Cổng do Azure App Service cấp
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`✅ Angular app is running on port ${port}`);
 });
